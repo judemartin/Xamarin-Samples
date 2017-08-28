bc75a1c1dfa3696714c8ae18fc7ca73db692818f
Jude Martin <judemartin.mj@gmail.com>
2017-08-27 21:57:34 -0700

    Entry Form

---
+++ EntryForm/EntryForm/EntryForm/EntryFormPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EntryForm
+{
+    public partial class EntryFormPage : ContentPage
+    {
+        public EntryFormPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSubmitButtonClicked(object sender, EventArgs e)
+        {
+            PersonalInformation personalnformation = (PersonalInformation)TableView.BindingContext;
+            summaryLabel.Text =
+                $"{personalnformation.Name} is {personalnformation.Age} years old, and has an email address " +
+                $"of {personalnformation.EmailAddress}, and a phone number of {personalnformation.PhoneNumber}, and is {(personalnformation.IsProgrammer ? "" : "not")}" +
+                "a programmer.";
+        }
+    }
+}
---
+++ EntryForm/EntryForm/EntryForm/PersonalInformation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryForm
+{
+    #region ViewModel
+    #endregion
+    class PersonalInformation : ViewModelBase
+    {
+        private string name, emailAddress, phoneNumber;
+        int age;
+        bool isProgrammer;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        public string EmailAddress
+        {
+            get { return emailAddress; }
+            set { SetProperty(ref emailAddress, value); }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { SetProperty(ref phoneNumber, value); }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { SetProperty(ref age, value); }
+        }
+        public bool IsProgrammer { get { return isProgrammer; } set { SetProperty(ref isProgrammer, value); } }
+    }
+}
